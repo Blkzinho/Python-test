152a5ff12b971c738a97c86921064be2f23c8eac
Fabio Santos <33484131+Blkzinho@users.noreply.github.com>
2023-04-21 16:11:54 +0200

    test for and MAIN

--- Tests Python/test For.py
+++ Tests Python/test For.py
@@ -1,11 +1,15 @@
 
-#for i in range(10):
-#    print(i,'bonjour')
+for i in range(10):
+    print(i,'for in range 10')
     
-#for element in range(2,10,3): # numero de debut , Numero max , De combien de pas je saute
-#    print(element,'bonjour')
+for element in range(2,10,3): # numero de debut , Numero max , De combien de pas je saute
+    print(element,'For in range starting at 2 at jump by 3')
 
 
 for element in range(10,-10,-1):
-    print(element,'bonjour')
-
+    print(element,'For in range from 10 to -10 backwards')
+    
+x = 0
+while x<=10:
+    print(x , 'While')
+    x += 1
