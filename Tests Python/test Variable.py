62acc4e74a6cc5b53f87bc2a43d5ac72e9e15f74
Fabio Santos <33484131+Blkzinho@users.noreply.github.com>
2023-04-21 08:16:17 +0200

    Test variable

---
+++ Tests Python/test Variable.py
@@ -0,0 +1,11 @@
+x = 1;
+print("La valeur de la variable est :");
+print(x);
+
+x = 2;
+print("La valeur de la variable est :");
+print(x);
+
+x = x + 1;
+print("et maintenant la valeur de la variable est :");
+print(x);
