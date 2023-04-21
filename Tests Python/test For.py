c4071b3dc68524a845b4209d58559184c4db1468
Fabio Santos <33484131+Blkzinho@users.noreply.github.com>
2023-04-21 16:07:18 +0200

    Test ifElse for python

---
+++ Tests Python/test For.py
@@ -0,0 +1,11 @@
+
+#for i in range(10):
+#    print(i,'bonjour')
+    
+#for element in range(2,10,3): # numero de debut , Numero max , De combien de pas je saute
+#    print(element,'bonjour')
+
+
+for element in range(10,-10,-1):
+    print(element,'bonjour')
+
